36e4559f902e4b1c2de38c13636867108c0fac28
BEOBMA <ssdssdtkfk@naver.com>
2024-11-08 23:20:09 +0900

    플러그인 추가

---
+++ src/main/kotlin/org/beobma/projectRhythm/ProjectRhythm.kt
@@ -0,0 +1,14 @@
+package org.beobma.projectRhythm
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ProjectRhythm : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
